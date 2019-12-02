f8494b1ee5a53d0e40e666111df1e50841a13cd8
avlisesac <casejsilva@gmail.com>
2019-12-02 15:48:41 -0500

    Adding additional folder structure

---
+++ app/modules/Colors.js
@@ -0,0 +1,11 @@
+const colors = {
+	ummnhDarkBlue: '#00274c',
+	ummnhLightBlue: '#00b6f1',
+	ummnhYellow: '#ffcb05',
+	ummnhLightGreen: '#a4d65e',
+	ummnhDarkGreen: '#007367',
+	ummnhLightRed: '#f03d69',
+	ummnhDarkRed: '#781f1c'
+}
+
+export default colors;
